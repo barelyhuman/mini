da221a76ce2f48e793e0fb2260445ad0f7f0f34a
reaper <ahoy@barelyhuman.dev>
2024-10-25 10:49:37 +0530

    feat: add promise map

--- bench.ts
+++ bench.ts
@@ -1,6 +1,7 @@
 import b from "npm:bluebird";
 import pmap from "npm:p-map";
 import { promiseMap } from "./promise-map.ts";
+import promiseFnsCreateQueue from "https://deno.land/x/promise_fns/src/queue.ts";
 
 const arrayOfItems = Array.from({ length: 100 }).fill(1) as number[];
 
@@ -25,5 +26,17 @@ Deno.bench({
   group: "Promise.map",
   async fn() {
     await pmap(arrayOfItems, async (d: number) => d * 2, { concurrency: 4 });
+  },
+});
+
+Deno.bench({
+  name: "promise_fns.queue",
+  group: "Promise.map",
+  async fn() {
+    const { add, queue } = promiseFnsCreateQueue<number>({ concurrency: 4 });
+    for (const ms of arrayOfItems) {
+      add(async () => ms * 2);
+    }
+    await queue;
   },
 });
